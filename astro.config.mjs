fe7b8a17a9206afafb4c8a8664df5c1a863fea39
Vasile Pop <vasile.pop@gmail.com>
2024-05-11 11:11:08 +0300

    add astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
@@ -14,10 +16,10 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), compressor({
     gzip: false,
     brotli: true
-  })],
+  }), icon()],
   output: "static",
   experimental: {
     clientPrerender: true,
     directRenderScript: true
-  },
+  }
 });
