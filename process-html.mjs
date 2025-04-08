04e5beec2adce903061ef58f563e20f81b0ce4b4
Vasile Pop <vasile.pop@gmail.com>
2025-04-08 16:26:03 +0300

    css animations

--- process-html.mjs
+++ process-html.mjs
@@ -8,7 +8,6 @@ const files = await globby(`${path}/**/*.html`);
 
 await Promise.all(
   files.map(async (file) => {
-   	 console.log('Processing file:', file);
    	 let html = await fs.readFile(file, 'utf-8');
 
    	 // Minify the HTML
