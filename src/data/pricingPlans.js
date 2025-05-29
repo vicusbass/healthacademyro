1b16390ad42b2e21657481ed4c3910cad011bd1e
Vasile Pop <vasile.pop@gmail.com>
2025-05-29 12:17:28 +0300

    add new version

---
+++ src/data/pricingPlans.js
@@ -0,0 +1,140 @@
+export const plans = [
+  {
+    name: "A. KINETOTERAPIE",
+    details: `
+**I. STANDARD**
+* 1 sedinta (1h) - 120 lei
+* 10 sedinte - 1000 lei (100 lei)
+* 20 sedinte - 1600 lei (80 lei)
+
+**II. PREMIUM**
+* 1 sedinta (max 3h) - 200 lei
+* 10 sedinte - 1750 lei (175 lei)
+* 20 sedinte - 3000 lei (150 lei)
+* On field
+
+    `
+  },
+  {
+    name: "B. FIZIOTERAPIE",
+    details: `
+**I. FIZIOTERAPIE STANDARD 3 PACK**
+* 1 sedinta - 150 lei
+* 10 sedinte - 1250 lei (125 lei)
+* 20 sedinte - 2000 lei (100 lei)
+
+**II. FIZIOTERAPIE INVAZIVA**
+(Dry, ElectroDry, EPI)
+* 1 sedinta - 250 lei
+* 4 sedinte - 800 lei
+
+**III. SERVICII SINGULARE**
+* TECAR / ShockWave / Stimulare electromagnetica - 80 lei
+* Laser / Crio-compresie -  60 lei
+* Electroterapie / Ultrasunet / Vaccum / Drenaj limfatic / CPM  40 lei 
+    `
+  },
+  {
+    name: "C. TERAPIE MANUALA",
+    details: `
+**I. TERAPIE MANUALA SEGMENT
+(MASAJ MEDICAL)**
+* 1 sedinta - 80 lei (15 min)
+* 10 sedinte - 650 lei (65 lei)
+* 20 sedinte - 1000 lei (50 lei)
+
+**II. TERAPIE MANUALA REGIUNE (MASAJ MEDICAL)**
+* 1 sedinta - 150 lei (45 min)
+* 10 sedinte - 1250 lei (125 lei)
+* 20 sedinte - 2000 lei (100 lei)
+
+**III. TERAPIE MANUALA PREMIUM**
+* 1 sedinta - 200 lei (1h)
+* 5 sedinte - 875 lei (175 lei)
+* 10 sedinte - 1500 lei (150 lei)
+    `
+  },
+  {
+    name: "D. EVALUARI / TESTARI",
+    details: `
+**I. EVALUARE**
+* clinica + ecografica: 300 lei;
+* posturala: 250 lei;
+
+**II. TESTARE**
+* forta musculara (izometrica, izokinetica, izotonica) - 300 lei
+* mobilitate - 100 lei
+* FMS - 100 lei
+* Vald ForceDecks - 300 lei
+* testare neurocognitiva - 300 lei
+* Green Room - 400 lei
+* Return to run / train / sport - 600 lei
+    `
+  },
+  {
+    name: "E. COLOANA",
+    details: `
+* Elongatii coloana Triton - 100 lei
+* Mobilizari 3D coloana Manuthera 242C - 100 lei
+* Masaj medical coloana - 150 lei
+* Terapie manuala premium coloana - 200 lei
+* Diatermie Winback Back4 - 120 lei
+* Laser inalta intensitate LightForce 25W - 120 lei
+* Criosauna Cryoniq XC - 175 lei
+    `
+  },
+  {
+    name: "F. HOME/TELE - HEALTH ",
+    details: `
+* Kinetoterapie domiciliu / birou(1h) - 200 lei
+* Fizioterapie domiciliu / birou (electro / TECAR / ultrasunet) - 250 lei
+* Masaj medical segment - 100 lei
+* Masaj medical regiune - 200 lei
+* Consultatie / Evaluare online - 300 lei
+* Program kinetoterapie personalizat online + 1 testare - 1200 lei / luna
+    `
+  },
+  {
+    name: "G. SERVICII MEDICALE",
+    details: `
+* Consultație ortopedie: 300 lei
+* Control ortopedie: 150 lei
+* Ablație fire: 100 lei
+* Pansament steril: 50 lei
+* Puncție ecoghidata: 250 lei
+* Infiltrație antiinflamator: 300 lei
+* Infiltrație soluție vascoelastică: 1000 lei
+* Infiltrație PRP: 1200 lei
+    `
+  },
+  {
+    name: "H. PREGATIRE SPORTIVA",
+    details: `
+**I. PREGATIRE STANDARD**
+* 1 sedinta (1h grup) - 80 lei
+* 10 sedinte - 600 lei (60 lei)
+
+**II. PREGATIRE PREMIUM**
+* 1 sedinta (individualizat) - 150 lei
+* 10 sedinte - 1250 lei (125 lei)
+
+**III. REFACERE STANDARD (RECOVERY)**
+(cryo-boots, electrostim, IASTM, SGA)
+* 1 sedinta - 100 lei
+* 10 sedinte - 800 lei
+
+**IV. REFACERE PREMIUM (RECOVERY+)**
+(criosauna, masaj membre / spate)
+* 1 sedinta - 300 lei
+* 10 sedinte - 2500 lei
+* Abon 2 persoane 10 sedinte - 4000 lei
+
+**V. SERVICII SINGULARE**
+* 10 criosauna - 1500 lei
+* 10 masaj sportiv regiune - 1250 lei
+* Masaj full body - 200 lei
+* Skill Court - 150 lei
+* Testare forta musculara, FMS, viteza, pliometrie, neurocognitiva - 400 lei
+    `
+    }
+];
--- src/utils/ui.ts
+++
@@ -1,3 +0,0 @@
-export const languages = {
-  ro: 'Romanian',
-};
